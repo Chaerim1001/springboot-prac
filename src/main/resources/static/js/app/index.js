60946108d06563d90bf3740e2e48dccd03f136e2
chaerim <qkrdbsk28@naver.com>
2022-12-29 01:50:25 +0900

    feat(04): 게시글 수정 화면 생성

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -4,6 +4,9 @@ const main = {
         $('#btn-save').on('click', () => {
             _this.save();
         });
+        $('#btn-update').on('click', () => {
+            _this.update();
+        });
     },
     save: function () {
         const data = {
@@ -20,6 +23,27 @@ const main = {
             data: JSON.stringify(data)
         }).done(() => {
             alert('글이 등록되었습니다.');
+            window.location.href = '/';
+        }).fail((error) => {
+            alert(JSON.stringify(error));
+        })
+    },
+    update: function () {
+        const data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        const id = $('#id').val()
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(() => {
+            alert('글이 수정되었습니다.');
             window.location.href = '/';
         }).fail((error) => {
             alert(JSON.stringify(error));
