845552435b6c9ac28d89dd0a545ff071bd1d45e1
chaerim <qkrdbsk28@naver.com>
2022-12-30 11:20:25 +0900

    feat(05): @LoginUser 어노테이션 추가

---
+++ src/main/java/com/prac/springboot/config/auth/LoginUser.java
@@ -0,0 +1,11 @@
+package com.prac.springboot.config.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER) // 이 어노테이션이 생성될 수 있는 위치를 지정한다. --> PARAMETER: 메소드의 파라미터로 선언된 객체에서만 사용할 수 있다.
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser { // @interface: 이 파일을 어노테이션 클래스로 지정한다. --> LoginUser 어노테이션이 생성
+}
---
+++ src/main/java/com/prac/springboot/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,31 @@
+package com.prac.springboot.config.auth;
+
+import com.prac.springboot.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) { // supportsParameter: 컨트롤러 메소드의 특정 파라미터를 지원하는지 판단한다.
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+        return isLoginUserAnnotation && isUserClass; // @LoginUser 어노테이션이 붙어있는지, 파라미터 클래스 타입이 SessionUser.class인지
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        // resolveArgument: 파라미터에 전달할 객체를 생성함
+        return httpSession.getAttribute("user"); // --> 세션에서 객체를 가져옴
+    }
+}
