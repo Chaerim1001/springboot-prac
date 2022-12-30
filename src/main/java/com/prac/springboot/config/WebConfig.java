da3094dfde193daa45b6975595b3b6733e7249d3
chaerim <qkrdbsk28@naver.com>
2022-12-30 11:25:00 +0900

    feat(05): session 저장소 데이터베이스 추가

--- src/main/java/com/prac/springboot/config/WebConfig.java
+++ src/main/java/com/prac/springboot/config/WebConfig.java
@@ -1,5 +1,6 @@
-package com.prac.springboot.config.auth;
+package com.prac.springboot.config;
 
+import com.prac.springboot.config.auth.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
